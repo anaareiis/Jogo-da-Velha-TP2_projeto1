6bdbcb315acdddbb0d33582a62ab028a8327feb7
anaareiis <anarnascente@gmail.com>
2025-09-22 22:44:24 -0300

    Teste 15: Implementação mínima: Jogador 1 vence na diagonal secundária.

--- velha.cpp
+++ velha.cpp
@@ -30,6 +30,10 @@ int VerificaVelha(int velha[3][3]) {
 	if (velha[0][0] == 1 && velha[1][1] == 1 && velha[2][2] == 1) {
 		return 1;
 	}
+	// Diagonal secundária Jogador 1
+	if (velha[0][2] == 1 && velha[1][1] == 1 && velha[2][0] == 1) {
+    	return 1;
+	}
     return 0;
 }
 
