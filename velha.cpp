0acfaa901cccd1ab8167cd4c1b7d24a27f981971
anaareiis <anarnascente@gmail.com>
2025-09-23 08:36:02 -0300

    Teste 17: Refatoração: unificação de linhas e colunas  Jogador 1

--- velha.cpp
+++ velha.cpp
@@ -14,23 +14,15 @@
  */ 
 
 int VerificaVelha(int velha[3][3]) {
-    // Linhas Jogador 1
-    for (int i = 0; i < 3; i++) {
-        if (velha[i][0] == 1 && velha[i][1] == 1 && velha[i][2] == 1) {
-            return 1;
-        }
+    // Linhas e colunas Jogador 1
+    for (int i = 0; i < 3; ++i) {
+        if (velha[i][0] == 1 && velha[i][1] == 1 && velha[i][2] == 1) return 1;
+        if (velha[0][i] == 1 && velha[1][i] == 1 && velha[2][i] == 1) return 1;
     }
-    // Colunas Jogador 1
-    for (int j = 0; j < 3; j++) {
-        if (velha[0][j] == 1 && velha[1][j] == 1 && velha[2][j] == 1) {
-            return 1;
-        }
-    }
-	// Jogador 1 - diagonais
+	// Diagonais Jogador 1
     if ((velha[0][0] == 1 && velha[1][1] == 1 && velha[2][2] == 1) ||
-        (velha[0][2] == 1 && velha[1][1] == 1 && velha[2][0] == 1)) {
-        return 1;
-    }
+        (velha[0][2] == 1 && velha[1][1] == 1 && velha[2][0] == 1)) return 1;
+
     return 0;
 }
 
